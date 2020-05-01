b581f12b4f777e8593ab07e04b2919a2d697ce3e
Adam Rocska <adam.rocska@adams.solutions>
2020-05-01 13:44:26 +0200

    Just to be absolutely in sync with GitHub. This is just a sketch.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Sources/MaxMindDBReader/Decoder.swift
+++ Sources/MaxMindDBReader/Decoder.swift
@@ -7,15 +7,21 @@ import Foundation
 
 class Decoder {
 
-  private let pointerBaseByteSize: UInt
+  private let data:                Data
   private var pointerBase:         UInt
+  private let pointerBaseByteSize: UInt
   private lazy var switchByteOrder: Bool = { 0.littleEndian == 0 }()
 
-  init(pointerBase: UInt = 0) {
+  init(data: Data, pointerBase: UInt = 0) {
     precondition(log(Float(pointerBase)).truncatingRemainder(dividingBy: 8.0) == 0)
-
-    self.pointerBaseByteSize = UInt(log(Float(pointerBase)) / 8.0)
+    self.data = data
     self.pointerBase = pointerBase
+    self.pointerBaseByteSize = UInt(log(Float(pointerBase)) / 8.0)
+  }
+
+  func decode(from offset: Data.Index) {
+    let controlByte    = data[offset]
+    let typeIdentifier = controlByte >> 5
   }
 
 }
