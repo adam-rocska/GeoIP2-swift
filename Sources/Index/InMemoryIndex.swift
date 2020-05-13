0d905f82d5d7f5ffa5570b4d9a113ad64af1ba10
Adam Rocska <adam.rocska@adams.solutions>
2020-05-13 13:59:02 +0200

    Different approach to constructing the index.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Sources/Index/InMemoryIndex.swift
+++ Sources/Index/InMemoryIndex.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Metadata
 
-class InMemoryIndex<Pointer>: Index where Pointer: UnsignedInteger, Pointer: FixedWidthInteger {
+public class InMemoryIndex<Pointer>: Index where Pointer: UnsignedInteger, Pointer: FixedWidthInteger {
 
   private let metadata: Metadata
   private let tree:     [Node<Pointer>]
@@ -11,41 +11,27 @@ class InMemoryIndex<Pointer>: Index where Pointer: UnsignedInteger, Pointer: Fix
     self.tree = tree
   }
 
-  required convenience init(metadata: Metadata, stream createStream: @autoclosure () -> InputStream) {
-    var tree: [Node<Pointer>] = []
-    let stream                = createStream()
+  required public convenience init(metadata: Metadata, stream createStream: @autoclosure () -> InputStream) {
+    let stream = createStream()
     precondition(stream.streamStatus == Stream.Status.notOpen, "should have provided an untouched stream.")
+    let searchTreeSize = Int(metadata.searchTreeSize)
     stream.open()
-    defer { stream.close() }
-
-    let nodeByteSize = Int(metadata.nodeByteSize)
-    while stream.hasBytesAvailable {
-      let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: nodeByteSize)
-      defer { buffer.deallocate() }
-
-      let read    = stream.read(buffer, maxLength: nodeByteSize)
-      tree.append(Node(Data(bytesNoCopy: buffer, count: read, deallocator: .none)))
-      if tree.count == metadata.nodeCount { break }
-    }
-
+    let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: searchTreeSize)
+    let read   = stream.read(buffer, maxLength: searchTreeSize)
+    let tree: [Node<Pointer>] = Data(
+      bytesNoCopy: buffer,
+      count: read,
+      deallocator: .unmap
+    )
+      .chunked(into: Int(metadata.nodeByteSize))
+      .map({ Node($0) })
+    buffer.deallocate()
+    stream.close()
     precondition(tree.count == metadata.nodeCount)
-
-
-    print("nodeCount                : \(metadata.nodeCount)")
-    print("recordSize               : \(metadata.recordSize)")
-    print("ipVersion                : \(metadata.ipVersion)")
-    print("databaseType             : \(metadata.databaseType)")
-    print("languages                : \(metadata.languages)")
-    print("binaryFormatMajorVersion : \(metadata.binaryFormatMajorVersion)")
-    print("binaryFormatMinorVersion : \(metadata.binaryFormatMinorVersion)")
-    print("buildEpoch               : \(metadata.buildEpoch)")
-    print("description              : \(metadata.description)")
-    print("nodeByteSize             : \(metadata.nodeByteSize)")
-    print("searchTreeSize           : \(metadata.searchTreeSize)")
     self.init(metadata: metadata, tree: tree)
   }
 
-  func lookup(_ ip: IpAddress) -> Pointer? {
+  public func lookup(_ ip: IpAddress) -> Pointer? {
     var stack = LookupDirection.createLookupStack(of: ip.data)
     return nil
   }
