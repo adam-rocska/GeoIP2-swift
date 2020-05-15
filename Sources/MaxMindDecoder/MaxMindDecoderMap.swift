7132a933eca41efaf66a7aceeba90f62f58c1dae
Adam Rocska <adam.rocska@adams.solutions>
2020-05-15 14:32:12 +0200

    Extract refactor to prepare for pointer handling.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Sources/MaxMindDecoder/MaxMindDecoderMap.swift
+++ Sources/MaxMindDecoder/MaxMindDecoderMap.swift
@@ -2,13 +2,17 @@ import Foundation
 
 public extension MaxMindDecoder {
 
+  private func resolveKey(_ iterator: MaxMindIterator) -> String? {
+    guard let keyControlByte = iterator.next() else { return nil }
+    if keyControlByte.type != .utf8String { return nil }
+    let keyBinary = iterator.next(keyControlByte)
+    return decode(keyBinary)
+  }
+
   func decode(_ iterator: MaxMindIterator, size: Int) -> [String: Any] {
     var result: [String: Any] = [:]
     for _ in 0..<size {
-      guard let keyControlByte = iterator.next() else { break }
-      if keyControlByte.type != .utf8String { break }
-      let keyBinary = iterator.next(keyControlByte)
-      let key: String = decode(keyBinary)
+      guard let key: String = resolveKey(iterator) else { break }
 
       guard let valueControlByte = iterator.next() else { break }
       switch valueControlByte.type {
