966928cfd22a647ac99ff503a9a085ed5c6a827c
Adam Rocska <adam.rocska@adams.solutions>
2020-05-17 19:49:15 +0200

    Falling back to the original crap code that emerged from crap php scripters' logic. Too many edge cases and stupidities to bother with. Dumb design can't be made straight.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Tests/DecoderTests/UInt8.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-extension UInt8 {
-  var description: String {
-    let binaryBase = String(self, radix: 2)
-    return String(repeating: "0", count: 8 - binaryBase.count) + binaryBase
-  }
-}
