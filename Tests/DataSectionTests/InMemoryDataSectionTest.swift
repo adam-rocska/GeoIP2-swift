f3b1e5ceaf0da858eb35838223c5288fe08e673d
Adam Rocska <adam.rocska@adams.solutions>
2020-05-28 15:41:26 +0200

    Put a primitive assertion in place just to hold my ass until the library comes together. Will make the tests professional & sexy at a later point.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Tests/DataSectionTests/InMemoryDataSectionTest.swift
+++ Tests/DataSectionTests/InMemoryDataSectionTest.swift
@@ -39,11 +39,66 @@ class InMemoryDataSectionTest: XCTestCase {
   }
 
   func testLookup_returnsExpectedDictionary() {
-    let dataSection = InMemoryDataSection(
+    let dataSection    = InMemoryDataSection(
       metadata: InMemoryDataSectionTest.countryMetadata,
       stream: InputStream(fileAtPath: InMemoryDataSectionTest.countryFilePath)!
     )
-    print(dataSection.lookup(pointer: 9696))
+    let expectedResult = [
+      "continent": Payload.map(
+        [
+          "geoname_id": Payload.uInt32(6255148),
+          "names": Payload.map(
+            [
+              "ru": Payload.utf8String("Европа"),
+              "en": Payload.utf8String("Europe"),
+              "de": Payload.utf8String("Europa"),
+              "fr": Payload.utf8String("Europe"),
+              "ja": Payload.utf8String("ヨーロッパ"),
+              "pt-BR": Payload.utf8String("Europa"),
+              "es": Payload.utf8String("Europa"),
+              "zh-CN": Payload.utf8String("欧洲")]
+          ),
+          "code": Payload.utf8String("EU")
+        ]
+      ),
+      "country": Payload.map(
+        [
+          "names": Payload.map(
+            [
+              "es": Payload.utf8String("Hungría"),
+              "zh-CN": Payload.utf8String("匈牙利"),
+              "ru": Payload.utf8String("Венгрия"),
+              "ja": Payload.utf8String("ハンガリー共和国"),
+              "de": Payload.utf8String("Ungarn"),
+              "fr": Payload.utf8String("Hongrie"),
+              "pt-BR": Payload.utf8String("Hungria"),
+              "en": Payload.utf8String("Hungary")]
+          ),
+          "iso_code": Payload.utf8String("HU"),
+          "is_in_european_union": Payload.boolean(true),
+          "geoname_id": Payload.uInt32(719819)]
+      ),
+      "registered_country": Payload.map(
+        [
+          "is_in_european_union": Payload.boolean(true),
+          "names": Payload.map(
+            [
+              "es": Payload.utf8String("Hungría"),
+              "de": Payload.utf8String("Ungarn"),
+              "fr": Payload.utf8String("Hongrie"),
+              "en": Payload.utf8String("Hungary"),
+              "pt-BR": Payload.utf8String("Hungria"),
+              "ja": Payload.utf8String("ハンガリー共和国"),
+              "ru": Payload.utf8String("Венгрия"),
+              "zh-CN": Payload.utf8String("匈牙利")]
+          ),
+          "geoname_id": Payload.uInt32(719819),
+          "iso_code": Payload.utf8String("HU")
+        ]
+      )
+    ]
+
+    XCTAssertEqual(expectedResult, dataSection.lookup(pointer: 9696))
   }
 
 }
