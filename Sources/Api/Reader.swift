f92a0daf7931c881507dee4a0c9da2401e8462e8
Adam Rocska <adam.rocska@adams.solutions>
2020-05-30 16:58:03 +0200

    Impossible to design a protocol at this point. And there's just no way I would take that php scripter fantasy crap as the fundamentum for this lib, no matter how crippled already it is by inherited scripter stupidity.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Sources/Api/Reader.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public protocol Reader {
-
-  associatedtype Model
-
-  func lookup(_ ip: IpAddress) -> Model
-
-}
--- Sources/Api/ReaderFileBased.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-protocol ReaderFileBased: Reader {
-  var metadata: DbMetadata { get }
-}
