6cc09263628ef8bd82899851ede33a495627f65a
Adam Rocska <adam.rocska@adams.solutions>
2020-05-30 16:59:39 +0200

    Trying to get a grab on the bull's horn somehow.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

---
+++ Sources/Api/Reader/CityFileBased.swift
@@ -0,0 +1,21 @@
+import Foundation
+import protocol DBReader.Reader
+
+public class CityFileBased {
+
+  static let databaseType = DatabaseType.city
+
+  let dbReader: DBReader.Reader
+  public var metadata: DbMetadata { get { return dbReader.metadata } }
+
+  public init(dbReader: Reader) {
+    precondition(dbReader.metadata.databaseType == CityFileBased.databaseType.rawValue)
+    self.dbReader = dbReader
+  }
+
+  public func lookup(_ ip: IpAddress) -> CityModel? {
+    guard let dictionary = dbReader.get(ip) else { return nil }
+    print(dictionary)
+    return nil
+  }
+}
---
+++ Tests/ApiTests/Reader/CityFileBasedTest.swift
@@ -0,0 +1,60 @@
+import Foundation
+import XCTest
+@testable import Api
+
+import protocol DBReader.Reader
+import enum Index.IpAddress
+import enum Decoder.Payload
+@testable import struct Metadata.Metadata
+
+class CityFileBasedTest: XCTestCase {
+
+  let stubMetadata = Metadata(
+    nodeCount: 0,
+    recordSize: 0,
+    ipVersion: 0,
+    databaseType: DatabaseType.city.rawValue,
+    languages: [],
+    binaryFormatMajorVersion: 0,
+    binaryFormatMinorVersion: 0,
+    buildEpoch: 0,
+    description: [:],
+    metadataSectionSize: 0,
+    databaseSize: 0
+  )
+
+  func testMetadataForwarding() {
+    let mockReader    = MockReader(metadata: stubMetadata)
+    let cityFileBased = CityFileBased(dbReader: mockReader)
+    XCTAssertEqual(stubMetadata, cityFileBased.metadata)
+  }
+
+  func testLookup_returnsNilIfDbReaderDidntReturnResult() {
+    let expectedIp   = IpAddress.v4("127.0.0.1")
+    var getWasCalled = false
+    let mockReader = MockReader(metadata: stubMetadata) { address in
+      XCTAssertEqual(expectedIp, address)
+      getWasCalled = true
+      return nil
+    }
+    let cityFileBased = CityFileBased(dbReader: mockReader)
+    XCTAssertNil(cityFileBased.lookup(expectedIp))
+    XCTAssertTrue(getWasCalled)
+  }
+
+}
+
+class MockReader: DBReader.Reader {
+
+  let metadata: Metadata
+  private let mockGet: (IpAddress) -> [String: Payload]?
+
+  init(metadata: Metadata, mockGet: @escaping (IpAddress) -> [String: Payload]?) {
+    self.metadata = metadata
+    self.mockGet = mockGet
+  }
+
+  convenience init(metadata: Metadata) { self.init(metadata: metadata, mockGet: { _ in nil }) }
+
+  func get(_ ip: IpAddress) -> [String: Payload]? { return mockGet(ip) }
+}
