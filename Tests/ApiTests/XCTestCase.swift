98276352998d9104dca9b55c27b0a077625b35d0
Adam Rocska <adam.rocska@adams.solutions>
2020-05-30 16:54:59 +0200

    Added GeoLite DB files for testing purposes. Totally not cool for "unit" testing, but I gotta validate the concepts somehow. Tests will be refactored.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

--- Tests/ApiTests/XCTestCase.swift
+++ Tests/ApiTests/XCTestCase.swift
@@ -3,17 +3,45 @@ import XCTest
 
 extension XCTestCase {
 
-  var bundle: Bundle {
+  static var bundle: Bundle {
     get {
-      guard let currentFileUrl = URL(string: #file) else {
-        return Bundle.main
-      }
-
+      guard let currentFileUrl = URL(string: #file) else { return Bundle.main }
       guard let testBundle = Bundle(path: currentFileUrl.deletingLastPathComponent().path) else {
         return Bundle.main
       }
-
       return testBundle
+    }
+  }
+
+  var bundle: Bundle { get { return XCTestCase.bundle } }
+
+  private static var countryFilePath: String {
+    get {
+      guard let countryFilePath = bundle.path(
+        forResource: "GeoLite2-Country_20200421/GeoLite2-Country",
+        ofType: "mmdb"
+      ) else { fatalError("GeoLite2 Country DB file was not found.") }
+      return countryFilePath
+    }
+  }
+
+  private static var cityFilePath: String {
+    get {
+      guard let countryFilePath = bundle.path(
+        forResource: "GeoLite2-City_20200526/GeoLite2-City",
+        ofType: "mmdb"
+      ) else { fatalError("GeoLite2 Country DB file was not found.") }
+      return countryFilePath
+    }
+  }
+
+  private static var asnFilePath: String {
+    get {
+      guard let countryFilePath = bundle.path(
+        forResource: "GeoLite2-ASN_20200526/GeoLite2-ASN",
+        ofType: "mmdb"
+      ) else { fatalError("GeoLite2 Country DB file was not found.") }
+      return countryFilePath
     }
   }
 
