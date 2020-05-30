5da03ef59d66da2d6cdb5d3aa2b9b304e48cc921
Adam Rocska <adam.rocska@adams.solutions>
2020-05-30 13:27:36 +0200

    DataBaseType enum for somehow handling the possiblly valid database metadata types.
    Signed-off-by: Adam Rocska <adam.rocska@adams.solutions>

---
+++ Sources/Api/Reader/DatabaseType.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum DatabaseType: String {
+  case city           = "City"
+  case country        = "Country"
+  case anonymousIp    = "GeoIP2-Anonymous-IP"
+  case asn            = "GeoLite2-ASN"
+  case connectionType = "GeoIP2-Connection-Type"
+  case domain         = "GeoIP2-Domain"
+  case enterprise     = "Enterprise"
+  case isp            = "GeoIP2-ISP"
+}
